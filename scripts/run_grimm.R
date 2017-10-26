2305a4ae5b5479e9db1aa5c50d9692355e9ef428
seaman248 <semen.bko@ya.ru>
2017-10-26 12:22:01 +0700

    run grimm script

--- scripts/run_grimm.R
+++ scripts/run_grimm.R
@@ -3,9 +3,13 @@ project_dir <- getwd()
 grimm_input <- paste0(project_dir, '/data/processed/grimm_input/grimm_input.tsv')
 grimm_output <- paste0(project_dir, '/data/processed/grimm_output')
 n_anchors <- 2
-min_support <- 60
 gap <- 115000
 
-command <- paste0('~/Documents/GRIMM_SYNTENY-2.02/grimm_synt -f ', grimm_input, ' -d ', grimm_output,' -n ', n_anchors,' -M ', min_support,' -g ' , gap)
+command <- paste0(
+  '~/Documents/GRIMM_SYNTENY-2.02/grimm_synt -f ', grimm_input,
+  ' -d ', grimm_output,
+  ' -n ', n_anchors,
+  ' -g ' , gap
+)
 
 system(command)
