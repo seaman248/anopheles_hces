87488499b6f6ae58878595fc942f26e23b56660c
seaman248 <semen.bko@ya.ru>
2017-10-26 12:21:27 +0700

    num

--- main.R
+++ main.R
@@ -1,20 +1,21 @@
-# Remap coordinates of genes on scaffolds/chromosomes to coordinates on elements
+# 1. Remap coordinates of genes on scaffolds/chromosomes to coordinates on elements
 # Results in ./data/processed/genes_on_elements
 
 # source('./scripts/remap_scf_or_chr_to_elements.R')
 
 
-# Rename genes to uid base on orthologs table
+# 2. Rename genes to uid base on orthologs table
 # Results in ./data/processed/uid_genes_on_elements
 
 # source('./scripts/rename_genes_as_orthologs.R')
 
 
-# Make Table for Grimm
+# 3. Make Table for Grimm
 # Result in ./data/procesed/grimm_input
 
 # source('./scripts/make_grimm_table.R')
 
-# Run GRIMM
+# 4. Run GRIMM
 # source('./scripts/run_grimm.R')
 
+# 5. Parse GRIMM_Synteny
